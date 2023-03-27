ed7ac26e80664ec405b1be6d3e92b16096257925
jun6292 <c68254@gmail.com>
2023-03-27 12:30:57 +0900

    Add: solved Babbling_2

---
+++ Week2/Babbling_2/jwj.py
@@ -0,0 +1,10 @@
+def solution(babbling):
+    answer = 0
+    babble = ["aya", "ye", "woo", "ma"]
+    for i in babbling:
+        for j in babble:
+            if j + j not in i:
+                i = i.replace(j, '#')
+        if i.strip('#') == '':
+            answer += 1
+    return answer
