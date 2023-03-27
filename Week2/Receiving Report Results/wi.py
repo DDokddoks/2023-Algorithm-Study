8d36c07de8aff1d6848d606411147779f1367db6
kimwiseong <kwws919@gmail.com>
2023-03-27 12:27:40 +0900

    Add: Receiving Report Results

--- Week2/Receiving Report Results/wi.py
+++ Week2/Receiving Report Results/wi.py
@@ -1,10 +1,31 @@
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
-    reported = {id : 0 for id in id_list}
-    report = [i.split() for i in set(report)]
+    reported = {id: 0 for id in id_list}
+    report = [r.split() for r in set(report)]
+    
+    for u, r in report: reported[r] += 1
+    for u, r in report: 
+        if reported[r] >= k: answer[id_list.index(u)] += 1
 
-    for i in report: reported[i[1]] += 1
-    for i in report:
-        if reported[i[1]] >= k: answer[id_list.index(i[0])] += 1
+    return answer~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --
+    
+    
+   
 
-    return answer
+  
+
+
+
+
