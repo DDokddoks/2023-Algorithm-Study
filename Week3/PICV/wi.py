f0adc04e2ca464abf14365c63a6e25eb8922c50d
wiseong <103016585+kimwiseong@users.noreply.github.com>
2023-03-29 20:13:04 +0900

    Delete wi.py

--- Week3/PICV/wi.py
+++
@@ -1,8 +0,0 @@
-def to_day(date, vaild = 0):
-    year, month, day = map(int, date.split("."))
-    return int(year)*28*12 + int(month + vaild) * 28 + int(day)
-
-def solution(today, terms, privacies):
-    terms = {t[0]: int(t[2:]) for t in terms} 
-    today = to_day(today)
-    return [i+1 for i, p in enumerate(privacies) if today >= to_day(p[:10], terms[p[-1]])]
