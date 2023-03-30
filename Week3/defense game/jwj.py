cac8d8a920b0b68367f340393acf32d14b31109e
jun6292 <c68254@gmail.com>
2023-03-30 10:26:42 +0900

    Modify: solved defense game

--- Week3/defense game/jwj.py
+++ Week3/defense game/jwj.py
@@ -1,16 +1,31 @@
-from heapq import heappush, heappop # maxheap 사용
+import heapq
+
+# def solution(n, k, enemy):
+#     answer = 0
+#     e_heap = []
+#     e_sum = 0
+#     for i in enemy:
+#         heapq.heappush(e_heap, i)
+#         e_sum += i
+#         if e_sum > n:
+#             if k == 0:
+#                 break
+#             e_sum -= heapq.heappop(e_heap)
+#             k -= 1
+#         answer += 1
+#     return answer
 
 def solution(n, k, enemy):
     answer = 0
-    heap = []
+    e_heap = []
     e_sum = 0
     for i in enemy:
-        heappush(heap, i)
+        heapq.heappush(e_heap, -i)
         e_sum += i
         if e_sum > n:
             if k == 0:
                 break
-            e_sum -= heappop(heap)
+            e_sum += heapq.heappop(e_heap)
             k -= 1
         answer += 1
     return answer
