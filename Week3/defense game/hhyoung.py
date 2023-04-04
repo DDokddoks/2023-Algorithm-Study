580863843ee2035ec7c98b5b898b86138cb063e0
nicolao00 <nicolao00@naver.com>
2023-04-04 16:38:25 +0900

    Add: solved defense game

---
+++ Week3/defense game/hhyoung.py
@@ -0,0 +1,20 @@
+def solution(n, k, enemy):
+    answer = 0
+
+    sortE=[0]*len(enemy)
+    left=0
+    right=len(enemy)
+    while left<right:
+        mid=(left+right)//2
+        sortE[0:mid+1]=sorted(enemy[0:mid+1],reverse=True)
+        total=sum(sortE[k:mid+1])
+        if n > total:
+            left=mid+1
+            answer=mid+1
+        elif n < total:
+            right=mid
+        else: 
+            answer=mid+1
+            break
+
+    return answer
