8f025f105864131a39426e78a99e4ff13bd047a3
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-07-11 00:20:05 +0900

    Add: solved Rotate Parentheses

---
+++ Week5/Rotate Parentheses/taegyu.py
@@ -0,0 +1,22 @@
+def is_valid(rotated):
+    stk = []
+    
+    for p in rotated:
+        stk.append(p)
+        while (len(stk) > 1 and 
+            ((stk[-2] == '(' and stk[-1] == ')') or
+             (stk[-2] == '{' and stk[-1] == '}') or
+             (stk[-2] == '[' and stk[-1] == ']'))):
+            stk.pop()
+            stk.pop()
+             
+    return not stk
+
+def solution(s):
+    answer = 0
+    
+    for i in range(len(s)):
+        if is_valid(s[i:]+s[:i]):
+            answer += 1
+        
+    return answer
