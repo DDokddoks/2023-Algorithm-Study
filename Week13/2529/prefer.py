795db1e84fbe887a8dd0d998d104e7fa9be6d52e
prefer52 <tjsgh151@naver.com>
2023-09-05 17:35:02 +0900

    Add: Solved 2529

---
+++ Week13/2529/prefer.py
@@ -0,0 +1,14 @@
+from sys import stdin
+from itertools import permutations
+
+k = int(stdin.readline())
+signs = stdin.readline().split()
+bigger = [(i,i+1) if sign == '<' else (i+1, i) for i, sign in enumerate(signs)]
+sequences = list(permutations(range(10), k+1))
+    
+def result_seq(sequences):
+    for seq in sequences:
+        if all(seq[pair[0]] < seq[pair[1]] for pair in bigger):
+            return ''.join(list(map(str, seq)))
+    
+print(result_seq(sequences[::-1]) + '\n' + result_seq(sequences))
